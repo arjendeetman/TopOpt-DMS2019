41867517545ee3cf44557bb330b2d49d68ec433d
arjendeetman <info@arjendeetman.nl>
2020-07-29 11:30:50 +0200

    Added example files with floor slabs

---
+++ ExampleFiles/3.Floor slabs/SQUARE-EDGE-GRID1-0/MAIN.py
@@ -0,0 +1,65 @@
+#########################
+### LICENSE
+#########################
+
+"""
+TODO
+"""
+
+#########################
+### MAIN PROGRAM
+#########################
+
+# Main program
+def main():
+
+    # Loading modules
+    import numpy as np
+    import os
+
+    # Read parameters
+    ## Find path of script
+    path = os.path.dirname(os.path.realpath(__file__))
+    ## File
+    inputFile = os.path.join(path, r"INPUT", "parameters.csv")
+    ## Import parameters
+    parameters = np.genfromtxt(inputFile, delimiter=',')
+    ## Set parameters
+    volfrac = parameters[0]
+    penalty = parameters[1]
+    move = parameters[2]
+    lmin = parameters[3]
+    tolx = parameters[4]
+    kmax = parameters[5]
+    width = parameters[6]
+    height = parameters[7]
+    edof = parameters[8]
+    filtering = parameters[9]
+    freeze = parameters[10]
+    if parameters[11] == 1: 
+        save = True
+    else: 
+        save = False
+    if parameters[12] == 1: 
+        continuation = True
+    else: 
+        continuation = False
+
+    # Run optimization process
+    ## Truss
+    if edof == 3:
+        from TOPOPT import mainTruss
+        mainTruss(volfrac, penalty, move, lmin, tolx, kmax, width, height, filtering, freeze, save, continuation)
+    ## Beam
+    if edof == 6:
+        from TOPOPT import mainBeam
+        mainBeam(volfrac, penalty, move, lmin, tolx, kmax, width, height, filtering, freeze, save, continuation)
+
+
+#########################
+### RUN THE MAIN PROGRAM
+#########################
+
+# Run the main program
+if __name__ == "__main__":
+    main()
